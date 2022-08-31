4b1f948bba9929ac300584029d03fdac97ccea78
Sebastián Nicolás Gómez Pacielo <sngomez@atsistemas.com>
2022-08-31 11:44:56 +0200

    changes to prettier config file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <p>Hola</p>
+      <button className='btn btn-primary'></button>
     </div>
   );
 };
