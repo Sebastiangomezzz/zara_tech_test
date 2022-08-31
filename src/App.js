21f4b7073263653bab54bc32ebd234c940694a39
Sebastián Nicolás Gómez Pacielo <sngomez@atsistemas.com>
2022-08-31 11:46:00 +0200

    delete test jsx elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import './App.css';
 
 export const App = () => {
-  return (
-    <div className='App'>
-      <p>Hola</p>
-      <button className='btn btn-primary'></button>
-    </div>
-  );
+  return <div className='App'></div>;
 };
