3a7636c8588a2cc433b12692dace3fdf098c69fd
Sebastián Nicolás Gómez Pacielo <sngomez@atsistemas.com>
2022-09-08 12:48:11 +0200

    fix position of Loading Spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import Spinner from 'react-bootstrap/Spinner';
 export const Loading = () => {
   return (
     <Spinner
-      className='position-fixed top-0 end-0'
+      className='position-fixed top-50 end-50'
       animation='border'
       variant='success'
     />
