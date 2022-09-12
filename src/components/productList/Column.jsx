6367c27f7015663f1ea4b81532384582681bbb04
Sebuntu <sebastiangomezzz@gmail.com>
2022-09-12 11:21:50 +0200

    refactor Column component

--- src/components/productList/Column.jsx
+++ src/components/productList/Column.jsx
@@ -1,52 +1,58 @@
 import Col from 'react-bootstrap/Col';
 import { ListItemCard } from './ListItemCard';
 
+const breakPointConstants = {
+  XXL: 'xxl',
+  XS: 'xs',
+  DEFAULT: 'default'
+};
+const breakPointConfig = {
+  [breakPointConstants.XXL]: (phone) => (
+    <Col
+      data-testid='col'
+      className='justify-content-center'
+      key={phone.id}
+      lg={3}>
+      <ListItemCard
+        data-testid='list-item-card'
+        id={phone.id}
+        imageUrl={phone.imgUrl}
+        brand={phone.brand}
+        model={phone.model}
+        price={phone.price}
+      />
+    </Col>
+  ),
+  [breakPointConstants.XS]: (phone) => (
+    <Col
+      data-testid='col'
+      className='justify-content-center'
+      key={phone.id}
+      xs={12}>
+      <ListItemCard
+        id={phone.id}
+        imageUrl={phone.imgUrl}
+        brand={phone.brand}
+        model={phone.model}
+        price={phone.price}
+      />
+    </Col>
+  ),
+  [breakPointConstants.DEFAULT]: (phone) => (
+    <Col
+      key={phone.id}
+      className='justify-content-center'>
+      <ListItemCard
+        id={phone.id}
+        imageUrl={phone.imgUrl}
+        brand={phone.brand}
+        model={phone.model}
+        price={phone.price}
+      />
+    </Col>
+  )
+};
 export const Column = ({ breakPoint, phone }) => {
-  if (breakPoint === 'xxl') {
-    return (
-      <Col
-        data-testid='col'
-        className='justify-content-center'
-        key={phone.id}
-        lg={3}>
-        <ListItemCard
-          data-testid='list-item-card'
-          id={phone.id}
-          imageUrl={phone.imgUrl}
-          brand={phone.brand}
-          model={phone.model}
-          price={phone.price}
-        />
-      </Col>
-    );
-  } else if (breakPoint === 'xs') {
-    return (
-      <Col
-        data-testid='col'
-        className='justify-content-center'
-        key={phone.id}
-        xs={12}>
-        <ListItemCard
-          id={phone.id}
-          imageUrl={phone.imgUrl}
-          brand={phone.brand}
-          model={phone.model}
-          price={phone.price}
-        />
-      </Col>
-    );
-  } else
-    return (
-      <Col
-        key={phone.id}
-        className='justify-content-center'>
-        <ListItemCard
-          id={phone.id}
-          imageUrl={phone.imgUrl}
-          brand={phone.brand}
-          model={phone.model}
-          price={phone.price}
-        />
-      </Col>
-    );
+  const config = breakPointConfig[breakPoint] || breakPointConfig[breakPointConstants.DEFAULT];
+  return config(phone);
 };
