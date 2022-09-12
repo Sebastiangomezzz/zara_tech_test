bdaee654597bd6102a54d9f717d1e74a21d7b0a8
Sebuntu <sebastiangomezzz@gmail.com>
2022-09-12 12:13:52 +0200

    add tests to ProductsDetailPage

--- src/__tests__/pages/ProductDetailPage.test.jsx
+++ src/__tests__/pages/ProductDetailPage.test.jsx
@@ -1,13 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import { ProductListPage } from '../../pages';
+import { render, screen, waitFor } from '@testing-library/react';
+import { ProductDetailPage } from '../../pages';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { reducers } from '../../store/store';
 import { productsApi } from '../../store/api/productsApi';
 import { MemoryRouter } from 'react-router-dom';
 
-describe('ProductListPage tests', () => {
-  test('Should render the ProductListPage component', () => {
+describe('ProductDetailPage tests', () => {
+  test('Should render the ProductDetailPage component', () => {
     const store = configureStore({
       reducer: reducers,
       middleware: (getDefaultMiddleware) =>
@@ -19,7 +19,9 @@ describe('ProductListPage tests', () => {
     });
     const { container } = render(
       <Provider store={store}>
-        <ProductListPage />
+        <MemoryRouter>
+          <ProductDetailPage />
+        </MemoryRouter>
       </Provider>
     );
     expect(container).toBeTruthy();
@@ -36,31 +38,13 @@ describe('ProductListPage tests', () => {
     });
     const { container } = render(
       <Provider store={store}>
-        <ProductListPage />
+        <MemoryRouter>
+          <ProductDetailPage />
+        </MemoryRouter>
       </Provider>
     );
     expect(container.firstChild.className).toBe(
       'position-fixed top-50 end-50 spinner-border text-success'
     );
-  });
-  test('should render a list of products', async () => {
-    const store = configureStore({
-      reducer: reducers,
-      middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-          serializableCheck: {
-            ignoredActions: ['persist/PERSIST']
-          }
-        }).concat(productsApi.middleware)
-    });
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <ProductListPage />
-        </MemoryRouter>
-      </Provider>
-    );
-    const products = await screen.findAllByTestId('list-item-card');
-    expect(products.length).toBe(100);
   });
 });
